af1ac40128721cadfcfe170a964f48eab16d39bf
Phon1209 <ptphon@gmail.com>
2022-08-20 05:53:38 -0400

    Optimize QR library
    - change to referencing from copying in QR_Code_create

--- main/ILI9341_QRCODE.cpp
+++ main/ILI9341_QRCODE.cpp
@@ -11,50 +11,48 @@ ILI9341_QRcode::ILI9341_QRcode(Adafruit_ILI9341 *TFTLCD)
 	this->LCD = TFTLCD;
 }
 
-void ILI9341_QRcode::QR_Code_create(String message, int display_x, int display_y)
+void ILI9341_QRcode::QR_Code_create(String &message, int display_x, int display_y)
 {
+	Serial.println("Start Create QR Code.....");
 	if (message.length() > 260)
 	{
-		Serial.println("Message lenght over 260 char!!");
+		Serial.println("Text is longer than 260 chars");
+		return;
 	}
-	else
-	{
-		Serial.println("Start Create QR Code.....");
-		// create QR code
-		message.toCharArray((char *)strinbuf, 260);
-		qrencode();
+	// create QR code
+	message.toCharArray((char *)strinbuf, 260);
+	qrencode();
 
-		for (byte x = 0; x < WD; x++)
+	for (byte x = 0; x < WD; x++)
+	{
+		for (byte y = 0; y < WD; y++)
 		{
-			for (byte y = 0; y < WD; y++)
+			if (QRBIT(x, y) && QRBIT((x + 1), y))
 			{
-				if (QRBIT(x, y) && QRBIT((x + 1), y))
-				{
-					// black square on top of black square
-					Serial.print("@");
-					LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_BLACK);
-				}
-				if (!QRBIT(x, y) && QRBIT((x + 1), y))
-				{
-					// white square on top of black square
-					Serial.print(" ");
-					LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_WHITE);
-				}
-				if (QRBIT(x, y) && !QRBIT((x + 1), y))
-				{
-					// black square on top of white square
-					Serial.print("@");
-					LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_BLACK);
-				}
-				if (!QRBIT(x, y) && !QRBIT((x + 1), y))
-				{
-					// white square on top of white square
-					Serial.print(" ");
-					LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_WHITE);
-				}
+				// black square on top of black square
+				Serial.print("@");
+				LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_BLACK);
 			}
-			Serial.println();
+			if (!QRBIT(x, y) && QRBIT((x + 1), y))
+			{
+				// white square on top of black square
+				Serial.print(" ");
+				LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_WHITE);
+			}
+			if (QRBIT(x, y) && !QRBIT((x + 1), y))
+			{
+				// black square on top of white square
+				Serial.print("@");
+				LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_BLACK);
+			}
+			if (!QRBIT(x, y) && !QRBIT((x + 1), y))
+			{
+				// white square on top of white square
+				Serial.print(" ");
+				LCD->fillRect(display_x + (x * piexl_size), display_y + (y * piexl_size), piexl_size, piexl_size, ILI9341_WHITE);
+			}
 		}
 		Serial.println();
 	}
+	Serial.println();
 }
--- main/ILI9341_QRCODE.h
+++ main/ILI9341_QRCODE.h
@@ -3,11 +3,11 @@
 
 class ILI9341_QRcode
 {
-	private:
-		Adafruit_ILI9341 *LCD;
-		//void render(int x, int y, int color);
+private:
+	Adafruit_ILI9341 *LCD;
+	//void render(int x, int y, int color);
 
-	public:
-		ILI9341_QRcode(Adafruit_ILI9341 *TFTLCD);
-		void QR_Code_create(String message, int display_x, int display_y );
+public:
+	ILI9341_QRcode(Adafruit_ILI9341 *TFTLCD);
+	void QR_Code_create(String &message, int display_x, int display_y);
 };
